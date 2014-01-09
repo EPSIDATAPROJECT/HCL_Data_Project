ba6b8b40ae6633feaa1d6a4646b15712b91971e2
Zozeph <josephrouffet@mbp-de-joseph.epsi.lyon.fr>
2014-01-09 12:32:27 +0100

    Des modif !!!!

---
+++ __OLD/include.php
@@ -0,0 +1,13 @@
+<?php
+// CONSTANTES
+define("PATH_CHARTS", "/lists/charts/");
+define("PATH_REPORTS", "/lists/reports/");
+define("PATH_SCRIPTS", "/scripts/");
+
+
+// CONNECTION BDD
+include PATH_SCRIPTS."connect.php";
+include PATH_SCRIPTS."request.php";
+
+
+
